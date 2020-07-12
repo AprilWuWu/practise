896f71d8ac2fdff618fef640157d55bdc453c7b0
吳靜雯 <april@wujingwende-iMac.local>
2020-07-12 21:11:44 +0800

    已修改 git commit -m 已修改

---
+++ main.py
@@ -0,0 +1,2 @@
+# print是方法 ，()是執行方法的意思
+print("Hello Pig!")
---
+++ p8.py
@@ -0,0 +1,34 @@
+print("撰寫第一支 Python 程式")
+
+print("練習一")
+
+print("print(3 + 2)")
+print("得出結果為")
+print(3 + 2)
+
+print("hello")
+# print後面的()在中文就是來承接丟進來資料的意思
+
+
+
+# p.10
+print("問題一")
+print("Decimal 是什麼？")
+
+# p.11
+print("練習二")
+
+# = 應用: 算出數字, 給名字 a
+a = 3 + 3.14
+print(a)
+
+# 重複利用 a, 並把結果叫做 b
+b = a * 2
+print(b)
+
+# 進階! 非常重要!
+# 把結果重新設定到 a 名字背後
+# 那舊的自然就不見了, 而是換成新設定的東西
+a = a * 3
+print(a)
+
